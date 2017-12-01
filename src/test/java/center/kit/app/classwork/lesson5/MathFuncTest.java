f2d08cb631c2d12b77f44332fecde7e3e8af9834
OlhaBorovyk <borovyk.olga@gmail.com>
2017-12-02 00:13:35 +0200

    Lesson 5 Homework Unit tests

---
+++ src/test/java/center/kit/app/classwork/lesson5/MathFuncTest.java
@@ -0,0 +1,24 @@
+package center.kit.app.classwork.lesson5;
+
+//    public class MathFuncTest {
+//        @Test
+//        public void multiplyTest(){
+//            int a = 2;
+//            int b = 1;
+//            int expRes = 2;
+//            MathFunc mathFunc = new MathFunc();
+//            int actResult = mathFunc.multiply(a, b);
+//            Assert.assertEquals(expRes, actResult);
+//        }
+//
+//        @Test
+//        public void multiplyDivTest(){
+//            int a = 8;
+//            int b = 4;
+//            int expRes = 2;
+//            MathFunc mathFunc = new MathFunc();
+//            int actResult = mathFunc.multiplyDiv(a, b);
+//            Assert.assertEquals(expRes, actResult);
+//        }
+//    }
+
